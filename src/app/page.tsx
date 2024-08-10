48262564cceef3b0627fbca9e437bfff80adafa7
ера <157960666+manyports@users.noreply.github.com>
2024-08-10 20:50:55 +0500

    Refactor page component to use consistent class name for text styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,8 +75,7 @@ export default function Component() {
           <div className="flex flex-col items-center space-y-4 text-center">
             <div className="space-y-2">
             <h1 
-              className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none improved-gradient-text"
-              data-text="Соединяем волонтеров и организации"
+              className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none text"
             >
               Соединяем волонтеров и организации
             </h1>
